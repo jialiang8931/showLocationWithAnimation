ef5c22019d9cab2a434678eb79063313c058e8c9
jialiang8931 <34622435+jialiang8931@users.noreply.github.com>
2021-05-04 16:32:53 +0800

    Add the button
    In order to increase the interaction between user and browser.

--- src/sketch.js
+++ src/sketch.js
@@ -12,12 +12,14 @@ let dataEvents;
 let infoWell;
 let date = new Date("1970-08-01")
 let plotDiameter;
+let sliderDate, sliderP;
+let bottomStop;
+let statusButtom = 1;
 
 function preload() {
   config = loadJSON("./config/config.json");
   layerCity = loadJSON('./assets/layer/huwei-city.geojson');
   dataWells = loadTable("./assets/layer/points.csv", 'header');
-  console.log("Dodfdddne!!!");
   dataEvents = loadTable("./assets/data/events.csv", 'header');
   console.log("Done!!!");
 }
@@ -27,6 +29,9 @@ function setup() {
   canvas = createCanvas(windowWidth*0.99, windowHeight*0.9);
   myMap = mappa.tileMap(config);
   myMap.overlay(canvas);
+  sliderDate = createSlider(0, dataEvents.rows.length-1, 0);
+  sliderP = createP("1970-08-01");
+  buttomStop = createButton("STOP");
   endP = createP("");
   dateP = createP("1970-08-01");
   dateP.position(70, 0); 
@@ -38,7 +43,15 @@ function setup() {
 
   shpCity = layerCity["features"][0]["geometry"]["coordinates"][0][0];
   myMap.onChange(onChangeDrawCity);
-  frameRate(12);
+
+  sliderDate.position(10, windowHeight*0.92);
+  sliderP.position(150, windowHeight*0.905);
+  sliderDate.input(plotDateEvent);
+
+  buttomStop.position(250, windowHeight*0.92);
+  buttomStop.mousePressed(stopAnimation);
+  frameRate(12)
+
 
 
 }
@@ -104,4 +117,69 @@ function draw() {
 
   // Show the frame rate of window.
   endP.html("frameRate:　" + round(frameRate()) + ";　days：" + (days+1));
+}
+
+
+
+function plotDateEvent(){
+  c = sliderDate.value();
+  clear();
+  drawCity();
+
+  // This part is trying to plot the location of well.
+  let totalDays = dataEvents.rows.length;
+  let totalWells = dataEvents.columns.length;
+  for (let i = 0; i < totalWells; i++) {
+    let well = dataEvents.columns[i].split(',');
+    let x = Number(well[1]); 
+    let y = Number(well[0]); 
+    let pos = myMap.latLngToPixel(y, x);
+    fill(0, 0, 0, 255);
+    strokeWeight(1);
+    ellipse(pos.x, pos.y, 6, 6);
+
+    fill(255, 255, 255, 255);
+    strokeWeight(1);
+    ellipse(pos.x, pos.y, 4, 4);
+
+  // This part is trying to plot events.
+    if (dataEvents.rows[days].get(dataEvents.columns[i]) === "1" ) {
+      let well = dataEvents.columns[i].split(',');
+      let x = Number(well[1]); 
+      let y = Number(well[0]); 
+      let pos = myMap.latLngToPixel(y, x);
+      fill(0, 0, 255, 125);
+      strokeWeight(1);
+      ellipse(pos.x, pos.y, 30*sin(PI/2));
+    }
+  }
+
+ // Show the date of events.
+  days = floor(c);
+  date = new Date("1970-08-01")
+  date.setDate(date.getDate() + days);
+  let M = date.getMonth()+1 >= 10 ? date.getMonth()+1 : "0" + (date.getMonth()+1);
+  let D = date.getDate() >= 10 ? date.getDate() : "0" + date.getDate();
+  dateP.html(date.getFullYear()+ '-' + M + '-' + D );
+  sliderP.html(date.getFullYear()+ '-' + M + '-' + D );
+  
+  // Show the frame rate of window.
+  endP.html("frameRate:　" + round(frameRate()) + ";　days：" + (days+1));
+  buttomStop.html("PLAY");
+  noLoop(); statusButtom = -1;
+
+}
+
+function stopAnimation(){
+  statusButtom *= -1
+  if (statusButtom === -1) {
+    buttomStop.html("PLAY");
+    noLoop();
+  } 
+
+  if (statusButtom === 1) {
+    buttomStop.html("STOP");
+    loop();
+  }
+
 }
