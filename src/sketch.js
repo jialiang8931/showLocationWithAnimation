9a4f061746d0ce28bb8ca66c44acfc216c5803d9
jialiang8931 <34622435+jialiang8931@users.noreply.github.com>
2021-05-04 17:25:36 +0800

    Add the one step buttom.

--- src/sketch.js
+++ src/sketch.js
@@ -13,7 +13,7 @@ let infoWell;
 let date = new Date("1970-08-01")
 let plotDiameter;
 let sliderDate, sliderP;
-let bottomStop;
+let buttomStop, buttomMove, buttomBack;
 let statusButtom = 1;
 
 function preload() {
@@ -32,6 +32,8 @@ function setup() {
   sliderDate = createSlider(0, dataEvents.rows.length-1, 0);
   sliderP = createP("1970-08-01");
   buttomStop = createButton("STOP");
+  buttomMove = createButton("＋");
+  buttomBack = createButton("－");
   endP = createP("");
   dateP = createP("1970-08-01");
   dateP.position(70, 0); 
@@ -48,9 +50,17 @@ function setup() {
   sliderP.position(150, windowHeight*0.905);
   sliderDate.input(plotDateEvent);
 
-  buttomStop.position(250, windowHeight*0.92);
+  buttomStop.position(300, windowHeight*0.92);
   buttomStop.mousePressed(stopAnimation);
+
+  buttomMove.position(260, windowHeight*0.92);
+  buttomMove.mousePressed(changeDay1);
+
+  buttomBack.position(230, windowHeight*0.92);
+  buttomBack.mousePressed(changeDay2);
+
   canvas.mousePressed(stopAnimation);
+
   frameRate(12)
 
 
@@ -183,4 +193,107 @@ function stopAnimation(){
     buttomStop.html("STOP");
     loop();
   }
+}
+
+
+
+
+function changeDay1() {
+  days = sliderDate.value() + 1;
+  c = days;
+  sliderDate.value(days);
+  clear();
+  drawCity();
+
+  // This part is trying to plot the location of well.
+  let totalDays = dataEvents.rows.length;
+  let totalWells = dataEvents.columns.length;
+  for (let i = 0; i < totalWells; i++) {
+    let well = dataEvents.columns[i].split(',');
+    let x = Number(well[1]); 
+    let y = Number(well[0]); 
+    let pos = myMap.latLngToPixel(y, x);
+    fill(0, 0, 0, 255);
+    strokeWeight(1);
+    ellipse(pos.x, pos.y, 6, 6);
+
+    fill(255, 255, 255, 255);
+    strokeWeight(1);
+    ellipse(pos.x, pos.y, 4, 4);
+
+  // This part is trying to plot events.
+    if (dataEvents.rows[days].get(dataEvents.columns[i]) === "1" ) {
+      let well = dataEvents.columns[i].split(',');
+      let x = Number(well[1]); 
+      let y = Number(well[0]); 
+      let pos = myMap.latLngToPixel(y, x);
+      fill(0, 0, 255, 125);
+      strokeWeight(1);
+      ellipse(pos.x, pos.y, 30*sin(PI/2));
+    }
+  }
+  
+ // Show the date of events.
+  date = new Date("1970-08-01")
+  date.setDate(date.getDate() + days);
+  let M = date.getMonth()+1 >= 10 ? date.getMonth()+1 : "0" + (date.getMonth()+1);
+  let D = date.getDate() >= 10 ? date.getDate() : "0" + date.getDate();
+  dateP.html(date.getFullYear()+ '-' + M + '-' + D );
+  sliderP.html(date.getFullYear()+ '-' + M + '-' + D );
+  
+  // Show the frame rate of window.
+  endP.html("frameRate:　" + round(frameRate()) + ";　days：" + (days+1));
+  buttomStop.html("PLAY");
+  noLoop(); statusButtom = -1;
+}
+
+
+
+function changeDay2() {
+  days = sliderDate.value() - 1;
+  c = days;
+  sliderDate.value(days);
+  clear();
+  drawCity();
+
+  // This part is trying to plot the location of well.
+  let totalDays = dataEvents.rows.length;
+  let totalWells = dataEvents.columns.length;
+  for (let i = 0; i < totalWells; i++) {
+    let well = dataEvents.columns[i].split(',');
+    let x = Number(well[1]); 
+    let y = Number(well[0]); 
+    let pos = myMap.latLngToPixel(y, x);
+    fill(0, 0, 0, 255);
+    strokeWeight(1);
+    ellipse(pos.x, pos.y, 6, 6);
+
+    fill(255, 255, 255, 255);
+    strokeWeight(1);
+    ellipse(pos.x, pos.y, 4, 4);
+
+  // This part is trying to plot events.
+    if (dataEvents.rows[days].get(dataEvents.columns[i]) === "1" ) {
+      let well = dataEvents.columns[i].split(',');
+      let x = Number(well[1]); 
+      let y = Number(well[0]); 
+      let pos = myMap.latLngToPixel(y, x);
+      fill(0, 0, 255, 125);
+      strokeWeight(1);
+      ellipse(pos.x, pos.y, 30*sin(PI/2));
+    }
+  }
+  
+ // Show the date of events.
+  date = new Date("1970-08-01")
+  date.setDate(date.getDate() + days);
+  let M = date.getMonth()+1 >= 10 ? date.getMonth()+1 : "0" + (date.getMonth()+1);
+  let D = date.getDate() >= 10 ? date.getDate() : "0" + date.getDate();
+  dateP.html(date.getFullYear()+ '-' + M + '-' + D );
+  sliderP.html(date.getFullYear()+ '-' + M + '-' + D );
+  
+  // Show the frame rate of window.
+  endP.html("frameRate:　" + round(frameRate()) + ";　days：" + (days+1));
+  buttomStop.html("PLAY");
+  noLoop(); statusButtom = -1;
 }
